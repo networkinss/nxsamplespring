3e22159380841536fa3803017990e1596f3c211c
networkinss <oliver.glas@inss.ch>
2024-08-02 04:04:58 +0200

    initial commit

---
+++ apps/apitestservice/src/main/java/com/example/demo/ApitestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitestserviceApplication.class, args);
+	}
+
+}
